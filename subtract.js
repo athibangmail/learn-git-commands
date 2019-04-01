67f20a5e614e50834f09f10e2c96da0911494564
athibanraj.t <athibanraj.t@gramener.com>
2019-04-01 05:59:36 +0530

    extra feature in subtract function

--- subtract.js
+++ subtract.js
@@ -1,6 +1,11 @@
 // Subtract.js
 
 // return subtraction of two numbers
-function subtract(a, b){
-    return a - b;
+function subtract(a, b, bMINUSa){
+    if(bMINUSa == true){
+        return a - b;
+    }
+    else{
+        return b -a;
+    }
 };
